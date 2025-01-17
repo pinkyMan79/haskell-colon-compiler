203d957a067b164a6cfdf4482939668e510124bf
danila <terenindanila5@gmail.com>
2025-01-17 05:45:30 +0300

    fix tokenizer

--- app/Main.hs
+++ app/Main.hs
@@ -1,59 +1,19 @@
 module Main (main) where
 
-import Data.Int (Int)
 import Control.Monad.State
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Char (isAsciiLower, isAsciiUpper)
 
 data NodeTypes =
-      CONST
-    | VAR
-    | ADD
-    | SUB
-    | MUL
-    | DIV
-    | LESSTHEN
-    | SET
-    | IF
-    | IFELSE
-    | WHILE
-    | SEQ
-    | EXPR
-    | PROG
-    | EMPTY
-    | PRINT
+      CONST | VAR | ADD | SUB | MUL | DIV
+    | LESSTHEN | SET | IF | IFELSE | WHILE
+    | SEQ | EXPR | PROG | EMPTY | PRINT
     deriving (Show, Eq)
 
 data TokenTypes =
-      Num
-    | Var
-    | If
-    | Else
-    | Lbra
-    | Rbra
-    | Lpar
-    | Rpar
-    | Add
-    | Sub
-    | Mul
-    | Div
-    | Less
-    | Assig
-    | Endfile
-    | Semicol
-    | Error
-    | Print
+      Num | Var | If | Else | While | Lbra | Rbra
+    | Lpar | Rpar | Add | Sub | Mul | Div | Less
+    | Assig | Endfile | Semicol | Error | Print
     deriving (Show, Eq)
-
--- 3 - node -> max case of 3 ops (IFELSE)
-data Node = Node {
-    nodeType :: NodeTypes,
-    value :: Maybe Int,
-    op1 :: Maybe Node,
-    op2 :: Maybe Node,
-    op3 :: Maybe Node
-} deriving (Show)
 
 data Token = Token {
     tokenType :: TokenTypes,
@@ -68,84 +28,84 @@ data CompilerState = CompilerState {
     tokens :: [Token]
 } deriving (Show)
 
-type CompilerM = State -> CompilerState
+type CompilerM = State CompilerState
 
 initCompilerState :: String -> CompilerState
-initCompilerState text = text (length text) 0 []
+initCompilerState inputText = CompilerState inputText (length inputText) 0 []
 
-tokenize :: CompilerM -> Bool
+tokenize :: CompilerM [Token]
 tokenize = do
-    token <- getNextToken
-    case token of
-        Token Endfile _ _ -> return true
-        Token Error _ _ -> return false
-        _ -> do
-        modify (\s -> s { tokens = tokens s ++ [token] })
-        tokenize
-
-nextChar :: Int -> String -> (Char, Int)
-nextChar pos str
-    | pos < length str = (str !! pos, pos + 1)
-    | otherwise = ('\0', pos) -- end line
+    state <- get
+    let pos = nowPos state
+        text' = text state
+        textLen' = textLen state
+    if pos >= textLen'
+        then return (tokens state)
+        else do
+            token <- getNextToken
+            state' <- get
+            put $ state' { tokens = tokens state' ++ [token] }
+            tokenize
 
 getNextToken :: CompilerM Token
 getNextToken = do
-  state <- get
-  let pos = nowPos state
-      text' = text state
-      textLen' = textLen state
-  if pos >= textLen' then return $ Token Endfile Nothing Nothing
-  else do
-    let (char, newPos) = nextChar pos text'
-    if isSpace char  -- skip spases recursively
-      then do
-        put $ state { nowPos = newPos }
-        getNextToken
-      else do
-        let (token, finalPos) = parseToken newPos char text'  -- Парсим токен
-        put $ state { nowPos = finalPos, tokens = tokens state ++ [token] }
-        return token
+    state <- get
+    let pos = nowPos state
+        text' = text state
+    if pos >= textLen state
+        then return $ Token Endfile Nothing Nothing
+        else do
+            let char = text' !! pos
+            if isSpace char
+                then do
+                    put $ state { nowPos = pos + 1 }
+                    getNextToken
+                else do
+                    let (token, finalPos) = parseToken pos char text'
+                    put $ state { nowPos = finalPos }
+                    return token
 
--- Функция для проверки на пробельный символ
-isSpace :: Char -> Bool
-isSpace c = c == ' ' || c == '\r' || c == '\t' || c == '\n'
-
--- Функция для парсинга токена
 parseToken :: Int -> Char -> String -> (Token, Int)
 parseToken pos char text'
-  | isDigit char = parseNumber pos char text'   -- Парсим число
-  | isLetter char = parseVariable pos char text'  -- Парсим переменную
-  | isSpecialSymbol char = parseSymbol pos char text' -- Парсим спецсимвол
-  | otherwise = (Token Error Nothing Nothing, pos + 1) -- Ошибка: неизвестный символ
+    | isDigit char = parseNumber pos text'
+    | isLetter char = parseIdentifier pos text'
+    | isSpecialSymbol char = parseSymbol pos char
+    | otherwise = (Token Error Nothing Nothing, pos + 1)
 
--- Парсинг числа
-parseNumber :: Int -> Char -> String -> (Token, Int)
-parseNumber pos char text' = do
-  let (numStr, finalPos) = span isDigit (drop pos text')
-      value = read numStr :: Int
-  (Token Num (Just value) Nothing, finalPos + pos)
+parseNumber :: Int -> String -> (Token, Int)
+parseNumber pos text' =
+    let (numStr, rest) = span isDigit (drop pos text')
+        value = read numStr
+    in (Token Num (Just value) Nothing, pos + length numStr)
 
+parseIdentifier :: Int -> String -> (Token, Int)
+parseIdentifier pos text' =
+    let (ident, rest) = span isIdentifierChar (drop pos text')
+        tokenType' = case ident of
+                       "if" -> If
+                       "else" -> Else
+                       "print" -> Print
+                       "while" -> While
+                       _ -> Var
+    in (Token tokenType' Nothing (if tokenType' == Var then Just (head ident) else Nothing), pos + length ident)
 
--- Парсинг переменной
-parseVariable :: Int -> Char -> String -> (Token, Int)
-parseVariable pos char text' = (Token Var Nothing (Just char) , pos + 1)
+parseSymbol :: Int -> Char -> (Token, Int)
+parseSymbol pos char
+    | char == '+' = (Token Add Nothing Nothing, pos + 1)
+    | char == '-' = (Token Sub Nothing Nothing, pos + 1)
+    | char == '*' = (Token Mul Nothing Nothing, pos + 1)
+    | char == '/' = (Token Div Nothing Nothing, pos + 1)
+    | char == '<' = (Token Less Nothing Nothing, pos + 1)
+    | char == '=' = (Token Assig Nothing Nothing, pos + 1)
+    | char == '(' = (Token Lpar Nothing Nothing, pos + 1)
+    | char == ')' = (Token Rpar Nothing Nothing, pos + 1)
+    | char == '{' = (Token Lbra Nothing Nothing, pos + 1)
+    | char == '}' = (Token Rbra Nothing Nothing, pos + 1)
+    | char == ';' = (Token Semicol Nothing Nothing, pos + 1)
+    | otherwise = (Token Error Nothing Nothing, pos + 1)
 
--- Парсинг спецсимвола (замените на свою логику)
-parseSymbol :: Int -> Char -> String -> (Token, Int)
-parseSymbol pos char text'
- | char == '+' = (Token Add Nothing Nothing, pos + 1)
- | char == '-' = (Token Sub Nothing Nothing, pos + 1)
- | char == '*' = (Token Mul Nothing Nothing, pos + 1)
- | char == '/' = (Token Div Nothing Nothing, pos + 1)
- | char == '<' = (Token Less Nothing Nothing, pos + 1)
- | char == '=' = (Token Assig Nothing Nothing, pos + 1)
- | char == '(' = (Token Lpar Nothing Nothing, pos + 1)
- | char == ')' = (Token Rpar Nothing Nothing, pos + 1)
- | char == '{' = (Token Lbra Nothing Nothing, pos + 1)
- | char == '}' = (Token Rbra Nothing Nothing, pos + 1)
- | char == ';' = (Token Semicol Nothing Nothing, pos + 1)
- | otherwise = (Token Error Nothing Nothing, pos + 1)
-
+isSpace :: Char -> Bool
+isSpace c = c `elem` " \r\n\t"
 
 isDigit :: Char -> Bool
 isDigit c = c >= '0' && c <= '9'
@@ -153,13 +113,15 @@ isDigit c = c >= '0' && c <= '9'
 isLetter :: Char -> Bool
 isLetter c = isAsciiLower c || isAsciiUpper c
 
+isSpecialSymbol :: Char -> Bool
+isSpecialSymbol c = c `elem` "+-*/<(){}=;"
 
-isSpecialSymbol :: Char -> Bool -- Здесь перечислены все спецсимволы вашего языка
-isSpecialSymbol c = c `elem` "+-*/<(){}="
-
+isIdentifierChar :: Char -> Bool
+isIdentifierChar c = isLetter c || isDigit c || c == '_'
 
 main :: IO ()
 main = do
-    let input = "{ a = 6; }"
-    initialState = initCompilerState input
-    result = evalState
+    let input = "{ a = 6; print 5; while (a < 10) { a = a + 10 } }"
+        initialState = initCompilerState input
+    let (tokens, _) = runState tokenize initialState
+    print tokens
